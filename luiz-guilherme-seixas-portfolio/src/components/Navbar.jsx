ab59b9df8db10712485ad539887853b72ed33db5
Luiz Guilherme S Seixas <guilhermesschiavon@gmail.com>
2023-06-16 20:28:31 -0300

    Realiza estilizações naNavbar

--- luiz-guilherme-seixas-portfolio/src/components/Navbar.jsx
+++ luiz-guilherme-seixas-portfolio/src/components/Navbar.jsx
@@ -1,24 +1,63 @@
 import React from 'react';
+import { TfiHome, TfiBook, TfiDesktop } from 'react-icons/tfi';
 import { useHistory } from 'react-router-dom';
 
 function Navbar() {
   const history = useHistory();
   return (
+    <header
+      className="
+      flex flex-col items-center md:flex-row md:justify-between
+      md:items-center py-4 px-8 bg-gray-100"
+    >
+      <div className="flex items-center mb-4 md:mb-0 md:mr-4 text-center">
+        <span
+          className="text-2xl md:text-xl sm:text-lg font-bold"
+        >
+          Luiz Guilherme Seixas
+        </span>
+      </div>
 
-    <nav>
-      <span>
-        Luiz G. Seixas
-      </span>
-      <button onClick={ () => history.push('/') }>
-        Home
-      </button>
-      <button onClick={ () => history.push('/about') }>
-        About
-      </button>
-      <button onClick={ () => history.push('/projects') }>
-        Projects
-      </button>
-    </nav>
+      <div className="flex flex-wrap justify-center md:justify-start space-x-4">
+        <button
+          onClick={ () => history.push('/') }
+          className="
+          flex items-center space-x-2 px-2 md:px-4 py-2
+          bg-blue-500 text-white rounded hover:bg-blue-600 mb-2 md:mb-0 h-full"
+        >
+          <TfiHome className="w-6 h-6" />
+          <span
+            className="hidden md:inline"
+          >
+            Home
+          </span>
+        </button>
+
+        <button
+          onClick={ () => history.push('/about') }
+          className="
+          flex items-center space-x-2 px-2 md:px-4 py-2
+          bg-blue-500 text-white rounded hover:bg-blue-600 mb-2 md:mb-0 h-full"
+        >
+          <TfiBook className="w-6 h-6" />
+          <span
+            className="hidden md:inline"
+          >
+            About
+          </span>
+        </button>
+
+        <button
+          onClick={ () => history.push('/projects') }
+          className="
+          flex items-center space-x-2 px-2 md:px-4 py-2
+          bg-blue-500 text-white rounded hover:bg-blue-600 flex-shrink-0 h-full"
+        >
+          <TfiDesktop className="w-6 h-6" />
+          <span className="hidden md:inline">Projects</span>
+        </button>
+      </div>
+    </header>
 
   );
 }
--- luiz-guilherme-seixas-portfolio/src/main.jsx
+++ luiz-guilherme-seixas-portfolio/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import AppProvider from './context/AppProvider';
+import '../index.css';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
