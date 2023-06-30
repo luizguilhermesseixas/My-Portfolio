d049581a20080ef872cd190bb70153c61d42882e
Luiz Guilherme S Seixas <guilhermesschiavon@gmail.com>
2023-06-30 00:30:24 -0300

    Realiza navBar e inicia Footer

--- luiz-guilherme-seixas-portfolio/src/components/Navbar.jsx
+++ luiz-guilherme-seixas-portfolio/src/components/Navbar.jsx
@@ -1,64 +1,103 @@
-import React from 'react';
-import { TfiHome, TfiBook, TfiDesktop } from 'react-icons/tfi';
+import React, { useState } from 'react';
+/* import { FaBars } from 'react-icons/fa'; */
+import { TfiHome, TfiBook, TfiDesktop, TfiMenu } from 'react-icons/tfi';
 import { useHistory } from 'react-router-dom';
 
 function Navbar() {
   const history = useHistory();
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header
-      className="
-      flex flex-col items-center md:flex-row md:justify-between
-      md:items-center py-4 px-8 bg-gray-100"
+      className="flex flex-col md:flex-row md:justify-between
+      md:items-center py-4 px-8 bg-[#f4f4f2]"
     >
-      <div className="flex items-center mb-4 md:mb-0 md:mr-4 text-center">
-        <span
-          className="text-2xl md:text-xl sm:text-lg font-bold"
-        >
-          Luiz Guilherme Seixas
-        </span>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <span
+            className="text-2xl text-[#243a69]
+          md:text-xl font-roboto"
+          >
+            Luiz Guilherme Seixas
+          </span>
+        </div>
+
+        <div className="flex items-center md:hidden">
+          <button onClick={ toggleMenu } className="p-2 text-[#243a69]">
+            <TfiMenu className="w-6 h-6" />
+          </button>
+        </div>
       </div>
 
-      <div className="flex flex-wrap justify-center md:justify-start space-x-4">
+      <div className="flex items-center space-x-4 md:ml-auto">
         <button
           onClick={ () => history.push('/') }
-          className="
-          flex items-center space-x-2 px-2 md:px-4 py-2
-          bg-blue-500 text-white rounded hover:bg-blue-600 mb-2 md:mb-0 h-full"
+          className="hidden md:flex items-center
+          space-x-2 px-2 py-2 border-b-4 border-transparent
+        text-[#5b88a5]  hover:border-[#243a69] hover:text-[#243a69]"
         >
           <TfiHome className="w-6 h-6" />
-          <span
-            className="hidden md:inline"
-          >
-            Home
-          </span>
+          <span className="hidden md:inline">Home</span>
         </button>
 
         <button
           onClick={ () => history.push('/about') }
-          className="
-          flex items-center space-x-2 px-2 md:px-4 py-2
-          bg-blue-500 text-white rounded hover:bg-blue-600 mb-2 md:mb-0 h-full"
+          className="hidden md:flex items-center
+          space-x-2 px-2 py-2 border-b-4 border-transparent
+        text-[#5b88a5]  hover:border-[#243a69] hover:text-[#243a69]"
         >
           <TfiBook className="w-6 h-6" />
-          <span
-            className="hidden md:inline"
-          >
-            About
-          </span>
+          <span className="hidden md:inline">About</span>
         </button>
 
         <button
           onClick={ () => history.push('/projects') }
-          className="
-          flex items-center space-x-2 px-2 md:px-4 py-2
-          bg-blue-500 text-white rounded hover:bg-blue-600 flex-shrink-0 h-full"
+          className="hidden md:flex items-center
+          space-x-2 px-2 py-2 border-b-4 border-transparent
+        text-[#5b88a5]  hover:border-[#243a69] hover:text-[#243a69]"
         >
           <TfiDesktop className="w-6 h-6" />
           <span className="hidden md:inline">Projects</span>
         </button>
       </div>
-    </header>
 
+      {isMenuOpen && (
+        <div className="md:hidden">
+          <div className="flex flex-col space-y-2 mt-2">
+            <button
+              onClick={ () => history.push('/') }
+              className="flex items-center space-x-2 text-[#243a69]"
+            >
+              <TfiHome className="w-6 h-6" />
+              <span className="ml-2">Home</span>
+            </button>
+            <div className="h-0.5 bg-[#243a69]" />
+
+            <button
+              onClick={ () => history.push('/about') }
+              className="flex items-center space-x-2 text-[#243a69]"
+            >
+              <TfiBook className="w-6 h-6" />
+              <span className="ml-2">About</span>
+            </button>
+            <div className="h-0.5 bg-[#243a69]" />
+
+            <button
+              onClick={ () => history.push('/projects') }
+              className="flex items-center space-x-2 text-[#243a69]"
+            >
+              <TfiDesktop className="w-6 h-6" />
+              <span className="ml-2">Projects</span>
+            </button>
+            <div className="h-0.5 bg-[#243a69]" />
+          </div>
+        </div>
+      )}
+    </header>
   );
 }
 
--- luiz-guilherme-seixas-portfolio/src/pages/Home.jsx
+++ luiz-guilherme-seixas-portfolio/src/pages/Home.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+// import SvgProgramming from '../components/SvgProgramming';
 
 function Home() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <h1>
-        HOMEPAGE
-      </h1>
+
+      <div className="flex flex-grow">
+        <div className="flex flex-col justify-center items-center w-full">
+          <h1 className="text-4xl font-roboto">
+            Olá, eu sou o Luiz Guilherme!
+          </h1>
+          <span className="text-2xl font-roboto">
+            Desenvolvedor Junior Full Stack
+          </span>
+        </div>
+      </div>
+
+      <Footer />
     </div>
   );
 }
